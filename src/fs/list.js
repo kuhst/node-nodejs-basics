cefeb443c584ee85c4f3edbfc09fc77b2a82779a
Oleg Kuharchuk <73923734+kuhst@users.noreply.github.com>
2022-11-30 23:49:03 +0300

    feat: implement list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,12 @@
+import { readdir } from 'node:fs/promises'
+import { checkDirExist } from "./utils/filesUtils.js";
+
+const dirPath = './files/';
+
 const list = async () => {
-    // Write your code here 
+    if (!await checkDirExist(dirPath)) throw Error('FS operation failed');
+
+    console.log(await readdir(dirPath));
 };
 
 await list();
