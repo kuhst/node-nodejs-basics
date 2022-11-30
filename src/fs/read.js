444d11b0879c969da285b8b91d9a9e987129bb63
Oleg Kuharchuk <73923734+kuhst@users.noreply.github.com>
2022-11-30 23:53:48 +0300

    feat: implement read function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,16 @@
+import { readdir, readFile } from 'node:fs/promises'
+import { checkDirExist } from "./utils/filesUtils.js";
+
+const dirPath = './files/';
+
 const read = async () => {
-    // Write your code here 
+    if (!await checkDirExist(dirPath)) throw Error('FS operation failed');
+
+    const files = await readdir(dirPath);
+    files.forEach(async (fileName) => {
+        const fileContent = await readFile(dirPath + fileName, 'utf-8');
+        console.log(fileContent)
+    })
 };
 
 await read();
