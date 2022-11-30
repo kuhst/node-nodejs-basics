c1d61f5458116fbb1c9d63657e7455b98fef7aa8
Oleg Kuharchuk <73923734+kuhst@users.noreply.github.com>
2022-11-30 23:21:47 +0300

    feat: implement copy function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,29 @@
+import { mkdir, readdir, readFile, writeFile } from 'node:fs/promises'
+import { checkDirExist } from "./utils/filesUtils.js";
+
+const dirPath = './files/';
+const copyDirPath = './files_copy/';
+
 const copy = async () => {
-    // Write your code here 
+    // check if "files" folder doesn't exists
+    if (!await checkDirExist(dirPath)) {
+        throw Error('FS operation failed');
+    }
+
+    // check if "files_copy" folder has already been created
+    if (await checkDirExist(copyDirPath)) {
+        throw Error('FS operation failed');
+    }
+
+    // create new dir
+    await mkdir(copyDirPath);
+
+    // get files from "dirPath"
+    const files = await readdir(dirPath);
+    files.forEach(async (fileName) => {
+        const fileContent = await readFile(dirPath + fileName);
+        await writeFile(copyDirPath + fileName, fileContent);
+    })
 };
 
 copy();
--- src/fs/utils/filesUtils.js
+++ src/fs/utils/filesUtils.js
@@ -1,8 +1,17 @@
-import { access } from 'node:fs/promises';
+import { access, readdir } from 'node:fs/promises';
 
 export const checkFileExist = async (filePath) => {
     try {
         await access(filePath)
+        return true
+    } catch (err) {
+        return false
+    }
+}
+
+export const checkDirExist = async (filePath) => {
+    try {
+        await readdir(filePath)
         return true
     } catch (err) {
         return false
